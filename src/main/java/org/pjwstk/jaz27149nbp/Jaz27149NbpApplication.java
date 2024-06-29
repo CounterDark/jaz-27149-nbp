84e97a22bdf8b1bf6981a68e0947861ec6163941
CounterDark <aleksanderkunkowski@gmail.com>
2024-06-29 12:36:51 +0200

    Init

---
+++ src/main/java/org/pjwstk/jaz27149nbp/Jaz27149NbpApplication.java
@@ -0,0 +1,13 @@
+package org.pjwstk.jaz27149nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz27149NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jaz27149NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pjwstk/jaz27149nbp/Jaz27149NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pjwstk.jaz27149nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz27149NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
